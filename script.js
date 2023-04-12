4cd181043c14b55ddd43d3357bc9e913a4d5c463
Yuliia <120779819+yuliiafedorchuk2@users.noreply.github.com>
2023-04-12 18:38:07 +0100

    add all files

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+document.getElementById("signInForm").addEventListener("submit",function(event) {
+    var name = document.getElementById("name").value;
+    var surname = document.getElementById("surname").value;
+    var password = document.getElementById("password").value;
+    
+    if (!name || name.length < 4) {
+        alert("Будь ласка введіть довше ім'я.");
+        return;
+    }
+    
+    if (!surname || surname.length < 4) {
+        alert("Будь ласка введіть довше прізвище.");
+        return;
+    }
+    
+    if (!password || password.length <= 7) {
+        alert("Будь ласка введіть правильний пароль, довжина якого має бути 8 або більше.");
+        return;
+    }
+});
+
 document.getElementById("signInForm").addEventListener("submit", function(event) {
     event.preventDefault(); 
 
